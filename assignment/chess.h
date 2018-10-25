27d5418533f96f8b356ad96b9f8621a5047d9f4c
StanislavSemashko <stanislavsemashko5@gmail.com>
2018-10-25 13:54:18 +0200

    First steps

--- assignment/chess.h
+++ assignment/chess.h
@@ -10,22 +10,32 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
-enum Color{White,Black}
+enum Color{White,Black};
+
 enum PieceType{
-  Pawn, Knight,Rook,Bishop,Queen,King
+  Pawn, Knight,Rook,Bishop,Queen,King, NoPiece
 };
+
 struct ChessPiece{
   enum Color color;
-  enum  ChessPiece piece;
-}
+  enum  PieceType type;
+};
 
 struct ChessSquare{
-  bool is_occupied:
+  bool is_occupied;
   struct ChessPiece piece;
 };
 
 typedef ChessSquare ChessBoard[8][8];
+typedef int File;
+typedef int Rank;
 
-bool is_piece(struct ChessPiece piece,enum Color color,enum PieceType type);
+bool is_piece(struct ChessPiece piece, enum Color color,enum PieceType type);
 
 void init_chess_board(ChessBoard chess_board);
+
+ChessSquare* get_square(ChessBoard board, int rank, int file);
+bool is_square_occupied(ChessBoard board, int rank, int file);
+bool add_piece(ChessBoard board, int rank, int file, struct ChessPiece piece);
+ChessPiece get_piece(ChessBoard board, int rank, int file);
+void setup_chess_board(ChessBoard board);
