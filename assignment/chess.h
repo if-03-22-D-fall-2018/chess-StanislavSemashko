5400f3bbbb1494dc1a2d59171f0a72e256ab9d64
StanislavSemashko <stanislavsemashko5@gmail.com>
2018-10-25 13:36:20 +0200

    Working

--- assignment/chess.h
+++ assignment/chess.h
@@ -10,3 +10,22 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+enum Color{White,Black}
+enum PieceType{
+  Pawn, Knight,Rook,Bishop,Queen,King
+};
+struct ChessPiece{
+  enum Color color;
+  enum  ChessPiece piece;
+}
+
+struct ChessSquare{
+  bool is_occupied:
+  struct ChessPiece piece;
+};
+
+typedef ChessSquare ChessBoard[8][8];
+
+bool is_piece(struct ChessPiece piece,enum Color color,enum PieceType type);
+
+void init_chess_board(ChessBoard chess_board);
